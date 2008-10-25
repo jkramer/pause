ebaa25fe78efb4ff9be8cb93ba74674dbd669417
Jonas Kramer <jkramer@nex.scrapping.cc>
2008-10-25 13:33:09 +0200

    Added CLI::Application::Plugin::RC::Hashify.

---
+++ CLI-Application-Plugin-RC-Hashify/lib/CLI/Application/Plugin/RC/Hashify.pm
@@ -0,0 +1,43 @@
+
+package CLI::Application::Plugin::RC::Hashify;
+
+use strict;
+use warnings;
+
+use Tie::File::Hashify;
+
+use CLI::Application::Plugin::RC;
+
+our @ISA = qw( CLI::Application::Plugin::RC );
+
+
+our $VERSION = '0.01';
+
+
+sub _load_rc {
+	my ($self, $path) = @_;
+
+	my %rc;
+
+	tie %rc, 'Tie::File::Hashify', parse => \&_parse;
+
+	$self->{rc} = { %rc };
+}
+
+
+sub _parse {
+	my ($line) = @_;
+
+	chomp $line;
+
+	$line =~ s/#.*$//;
+	
+	if($line =~ /\s*(\S+)\s*=\s*(.*?)\s*/) {
+		return ($1, $2);
+	}
+
+	return;
+}
+
+
+!0
